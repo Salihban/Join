2d536377864124966e01e7141b3b4bcdd6c57055
Salih Derin <derin.salih@gmx.de>
2026-08-10 14:27:07 +0200

    feat: add components

---
+++ join/src/app/components/contact-info/contact-info.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-info',
+  imports: [],
+  templateUrl: './contact-info.html',
+  styleUrl: './contact-info.scss',
+})
+export class ContactInfo {}
