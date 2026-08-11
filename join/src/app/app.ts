8ec6a7ce78a904e27372a084c2ff9ec82131fa56
MatzeRB26 <m.stojak14@gmail.com>
2026-08-11 15:52:33 +0200

    feat: privacy policy Page created

--- join/src/app/app.ts
+++ join/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LegalNotice } from './pages/legal-notice/legal-notice';
+import { PrivacyPolicy } from "./pages/privacy-policy/privacy-policy";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LegalNotice],
+  imports: [RouterOutlet, LegalNotice, PrivacyPolicy],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
