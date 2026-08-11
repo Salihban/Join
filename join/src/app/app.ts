111bee49620b78fc7b30143d1a40bc45b6b79657
MatzeRB26 <m.stojak14@gmail.com>
2026-08-11 16:32:10 +0200

    feat: help page create

--- join/src/app/app.ts
+++ join/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LegalNotice } from './pages/legal-notice/legal-notice';
 import { PrivacyPolicy } from "./pages/privacy-policy/privacy-policy";
+import { Help } from "./pages/help/help";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LegalNotice, PrivacyPolicy],
+  imports: [RouterOutlet, LegalNotice, PrivacyPolicy, Help],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
