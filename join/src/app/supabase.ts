eaf16e63924b2f79f81959b63f0959cf00a82b16
frontendfuchs <adriana.letitia@yahoo.com>
2026-08-18 13:53:35 +0200

    feat: implement addContact method

--- join/src/app/supabase.ts
+++ join/src/app/supabase.ts
@@ -14,20 +14,25 @@ export interface ContactGroup {
     contacts: Contact[];
 }
 
+export interface NewContact {
+    name: string;
+    email: string;
+    phone: string;
+}
+
 @Injectable({
     providedIn: 'root'
 })
+
 export class Supabase {
     readonly supabaseUrl: string = 'https://mbifznamhyihpgduvqru.supabase.co';
     readonly supabaseKey: string = 'sb_publishable_JUaeWi8_jhlIrDYKFIr-HQ_ZPNiPis2';
     readonly supabase = createClient(this.supabaseUrl, this.supabaseKey);
-
     readonly contacts = signal<Contact[]>([]);
     readonly groupedContacts = computed<ContactGroup[]>(() => {
         const sortedContacts = [...this.contacts()].sort((a, b) =>
             a.name.localeCompare(b.name, 'de')
         );
-
         const groups: ContactGroup[] = [];
 
         for (const contact of sortedContacts) {
@@ -59,5 +64,19 @@ export class Supabase {
 
         if (!contacts) return;
         this.contacts.set(contacts);
+    }
+
+
+    async addContact(contact: NewContact): Promise<string | null> {
+        const { error } = await this.supabase
+            .from('contacts')
+            .insert(contact);
+
+        if (error) {
+            console.error('Fehler beim Hinzufügen', error);
+            return 'The contact could not be added. It may already exist or contain invalid characters';
+        }
+        await this.getContacts();
+        return null;
     }
 }
