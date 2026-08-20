bc0860575db050038d04b30f892c98cb5d92447a
Salih Derin <derin.salih@gmx.de>
2026-08-20 12:37:30 +0200

    feat: pop up window for save and delete

--- join/src/app/components/contact-list/contact-list.ts
+++ join/src/app/components/contact-list/contact-list.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { Supabase } from '../../supabase';
 import { ContactDialog } from '../contact-dialog/contact-dialog';
 
@@ -9,10 +9,24 @@ import { ContactDialog } from '../contact-dialog/contact-dialog';
   templateUrl: './contact-list.html',
   styleUrl: './contact-list.scss'
 })
-export class ContactList {
+export class ContactList implements OnInit{
   dbService = inject(Supabase);
+  toastMessage = signal('');
 
   ngOnInit(): void {
     this.dbService.getContacts();
   }
+
+  onContactSaved() {
+        this.dbService.triggerToast('Contact succesfully created');
+    }
+
+    onContactDeleted() {
+        this.triggerToast('Contact Delete succsessfull');
+    }
+
+    private triggerToast(message: string) {
+        this.toastMessage.set(message);
+        setTimeout(() => this.toastMessage.set(''), 3000);
+    }
 }
