53f4952447b30a4a80c0d5a4a652ac1f1b6cc844
Salih Derin <derin.salih@gmx.de>
2026-08-27 16:00:22 +0200

    feat: generation components

---
+++ join/src/app/components/task-overlay/task-overlay.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TaskOverlay } from './task-overlay';
+
+describe('TaskOverlay', () => {
+    let component: TaskOverlay;
+    let fixture: ComponentFixture<TaskOverlay>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [TaskOverlay],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(TaskOverlay);
+        component = fixture.componentInstance;
+        await fixture.whenStable();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
