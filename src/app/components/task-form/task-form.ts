d58e1f3759607de58ab85c507d949533318ee1f7
Salih Derin <derin.salih@gmx.de>
2026-08-28 17:10:54 +0200

    implements Supabase

--- src/app/components/task-form/task-form.ts
+++ src/app/components/task-form/task-form.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Contact, Supabase } from '../../supabase';
 import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Title } from '@angular/platform-browser';
 
 type Priority = 'urgent' | 'medium' | 'low';
 
@@ -10,8 +10,37 @@ type Priority = 'urgent' | 'medium' | 'low';
     templateUrl: './task-form.html',
     styleUrl: './task-form.scss',
 })
-export class TaskForm {
+export class TaskForm implements OnInit{
     private formBuilder = inject(FormBuilder);
+    readonly dbService = inject(Supabase);
+    readonly contacts = this.dbService.contacts;
+
+    ngOnInit(): void {
+        this.dbService.getContacts();
+    }
+
+    toggleDropDown(): void {
+        this.dropdownOpen = !this.dropdownOpen;
+    }
+
+    toggleContact(contactId: string): void {
+        const control = this.taskForm.controls.assignedContactIds;
+        const ids = control.value;
+
+        control.setValue(
+            ids.includes(contactId)? ids.filter(id => id !== contactId): [...ids, contactId]
+        );
+    }
+
+    isSelected(contactId: string): boolean {
+        return this.taskForm.controls.assignedContactIds.value.includes(contactId);
+    }
+
+    get selectContacts(): Contact[] {
+        const ids = this.taskForm.controls.assignedContactIds.value;
+
+        return this.contacts().filter(contact => ids.includes(contact.id));
+    }
 
     taskForm = this.formBuilder.nonNullable.group({
         title: ['', Validators.required],
