1f19cd2437477965d3d337de79ace0cf25843543
MatzeRB26 <m.stojak14@gmail.com>
2026-08-28 15:47:30 +0200

    feat: built on the board side

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Aside } from './layout/aside/aside';
 import { Header } from './layout/header/header';
+import { Board } from "./pages/board/board";
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Aside],
+  imports: [RouterOutlet, Header, Aside, Board],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
