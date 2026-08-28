446aa0758eb5cdb49a28e8cbddd32f9244867642
Salih Derin <derin.salih@gmx.de>
2026-08-28 13:33:51 +0200

    feat: create TaskForm

--- src/app/components/task-form/task-form.ts
+++ src/app/components/task-form/task-form.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+
+type Priority = 'urgent' | 'medium' | 'low';
 
 @Component({
     selector: 'app-task-form',
-    imports: [],
+    imports: [ReactiveFormsModule],
     templateUrl: './task-form.html',
     styleUrl: './task-form.scss',
 })
-export class TaskForm {}
+export class TaskForm {
+    private formBuilder = inject(FormBuilder);
+
+    taskForm = this.formBuilder.nonNullable.group({
+        title: ['', Validators.required],
+        description: [''],
+        dueData: ['', Validators.required],
+        priority: ['medium' as Priority],
+        assignedContactIds: this.formBuilder.nonNullable.control<string[]>([]),
+        category: ['', Validators.required],
+        subtasks: this.formBuilder.array<FormControl<string>>([])
+    });
+
+    submitForm(): void {
+        if (this.taskForm.invalid) {
+            this.taskForm.markAllAsTouched();
+            return;
+        }
+    }
+
+    clearForm(): void {
+        this.taskForm.reset({
+            title: '',
+            description: '',
+            dueData: '',
+            priority: 'medium',
+            assignedContactIds: [],
+            category: ''
+        });
+    this.taskForm.controls.subtasks.clear();
+    }
+}
