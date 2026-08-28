4faf9db7635d59cd92a0ac2cf05d3f09bf488809
frontendfuchs <adriana.letitia@yahoo.com>
2026-08-28 13:15:51 +0200

    feat: add navigation route for Add Task page

--- src/app/pages/add-task/add-task.ts
+++ src/app/pages/add-task/add-task.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-add-task',
+    standalone: true,
     imports: [],
     templateUrl: './add-task.html',
     styleUrl: './add-task.scss',
