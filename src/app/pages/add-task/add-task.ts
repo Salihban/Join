e5c93e36c5fcda7ffa7c9be70b3b54116fd602a8
frontendfuchs <adriana.letitia@yahoo.com>
2026-09-01 04:02:12 +0200

    feat: implement and style add task form

--- src/app/pages/add-task/add-task.ts
+++ src/app/pages/add-task/add-task.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskForm } from '../../components/task-form/task-form';
 
 @Component({
     selector: 'app-add-task',
     standalone: true,
-    imports: [],
+    imports: [TaskForm],
     templateUrl: './add-task.html',
     styleUrl: './add-task.scss',
 })
