74d26efe0e172cb233e6602d03405189e43b996a
frontendfuchs <adriana.letitia@yahoo.com>
2026-09-04 00:47:03 +0200

    feat:add dialog component with close button and integrate task-form

--- src/app/components/task-overlay/task-overlay.ts
+++ src/app/components/task-overlay/task-overlay.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { TaskForm } from '../task-form/task-form';
 
 @Component({
     selector: 'app-task-overlay',
-    imports: [],
+    imports: [TaskForm],
     templateUrl: './task-overlay.html',
     styleUrl: './task-overlay.scss',
 })
-export class TaskOverlay {}
+export class TaskOverlay {
+    private dialogRef = inject(MatDialogRef<TaskOverlay>);
+
+    onTaskCreated():void {
+        console.log('[TaskFormDialog] Task created,closing dialog');
+        this.dialogRef.close(true);
+    }
+
+    onClose(): void {
+        console.log('[TaskFormDialog] Close button clicked');
+        this.dialogRef.close(false);
+    }
+}
