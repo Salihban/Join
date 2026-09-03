3420198e82ebb203474c5414e8deb6e58a20b2a5
Salih Derin <derin.salih@gmx.de>
2026-09-03 17:11:19 +0200

    create Task detail overly on display

--- src/app/pages/board/board.ts
+++ src/app/pages/board/board.ts
@@ -1,16 +1,18 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Task, TaskService } from '../../services/task';
 import { TaskCard } from '../../components/task-card/task-card';
+import { TaskDetails } from '../../components/task-details/task-details';
 
 @Component({
     selector: 'app-board',
-    imports: [TaskCard],
+    imports: [TaskCard, TaskDetails],
     templateUrl: './board.html',
     styleUrl: './board.scss',
 })
 export class Board implements OnInit{
     tasks = signal<Task[]>([]);
     private taskService = inject(TaskService);
+    selectedTask = signal<Task | null>(null);
 
     async ngOnInit(): Promise<void> {
         this.tasks.set(await this.taskService.getTasks());
