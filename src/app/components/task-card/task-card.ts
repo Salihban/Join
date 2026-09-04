cc918250e3176cdeb19ca80b6848bb3d9b28940c
Axel-ne <axel-neugebauer@gmx.net>
2026-09-04 14:14:27 +0200

    fix : initials

--- src/app/components/task-card/task-card.ts
+++ src/app/components/task-card/task-card.ts
@@ -9,24 +9,23 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     styleUrl: './task-card.scss',
 })
 export class TaskCard {
-    @Input({required: true}) task!: Task;
+    @Input({ required: true }) task!: Task;
+    
 
     get categoryName(): string {
-        return this.task.category === 'user_story'
-                ? 'User Story'
-                : 'Technical Task';
+        return this.task.category === 'user_story' ? 'User Story' : 'Technical Task';
     }
 
     get totalSubtasks(): number {
-    return this.task.subtasks?.length ?? 0;
+        return this.task.subtasks?.length ?? 0;
     }
 
     get completedSubtasks(): number {
-    return this.task.subtasks?.filter(subtask => subtask.completed).length ?? 0;
+        return this.task.subtasks?.filter((subtask) => subtask.completed).length ?? 0;
     }
 
     get progressValue(): number {
-    if (this.totalSubtasks === 0) return 0;
-    return (this.completedSubtasks / this.totalSubtasks) * 100;
+        if (this.totalSubtasks === 0) return 0;
+        return (this.completedSubtasks / this.totalSubtasks) * 100;
     }
 }
