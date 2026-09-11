34aa92a1b753b0061d51f1de36a554290ad918f7
MatzeRB26 <m.stojak14@gmail.com>
2026-09-11 14:48:41 +0200

    feat: Implemented form validation and a button to reveal the password

--- src/app/pages/log-in/log-in.ts
+++ src/app/pages/log-in/log-in.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-    imports: [],
     selector: 'app-log-in',
+    standalone: true,
+    imports: [CommonModule, ReactiveFormsModule],
     styleUrl: './log-in.scss',
     templateUrl: './log-in.html',
 })
-export class LogIn {}
+export class LogIn {
+    loginForm = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+
+    generalError: string = '';
+    submitted: boolean = false; 
+    passwordVisible: boolean = false; 
+
+    get email() { return this.loginForm.get('email'); }
+    get password() { return this.loginForm.get('password'); }
+
+    togglePasswordVisibility() {
+        const passwordValue = this.password?.value;
+        if (passwordValue && passwordValue.length > 0) {
+            this.passwordVisible = !this.passwordVisible;
+        }
+    }
+
+    onSubmit() {
+        this.submitted = true; 
+
+        if (this.loginForm.invalid) {
+            this.loginForm.markAllAsTouched();
+            return;
+        }
+        this.generalError = '';
+    }
+}
