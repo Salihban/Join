60e3f009801c817705ce85b0e26455b6f919ed02
Salih Derin <derin.salih@gmx.de>
2026-09-13 23:25:25 +0200

    feat: create Router Links Log-in

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router ,RouterOutlet } from '@angular/router';
 import { Aside } from './layout/aside/aside';
 import { Header } from './layout/header/header';
 import { ContactService } from './services/contact';
@@ -8,12 +8,13 @@ import { LogIn } from './pages/log-in/log-in';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Aside, LogIn],
+  imports: [RouterOutlet, Header, Aside],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 
 export class App {
   protected readonly title = signal('join');
+  public router = inject(Router);
   public contactService = inject(ContactService);
 }
