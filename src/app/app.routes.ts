a554a09bc6eee7c2aa3998381b39363e2643572b
Salih Derin <derin.salih@gmx.de>
2026-09-15 00:08:31 +0200

    feat: create Sign-up component and add sign-up route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,11 @@ export const routes: Routes = [
             import('./pages/log-in/log-in').then((m) => m.LogIn)
     },
     {
+        path: 'sign-up',
+        loadComponent: () =>
+            import('./pages/sign-up/sign-up').then((m) => m.SignUp)
+    },
+    {
         path: 'contacts',
         loadComponent: () =>
             import('./components/contacts-view/contacts-view').then(m => m.ContactsView)
--- src/app/pages/sign-up/sign-up.ts
+++ src/app/pages/sign-up/sign-up.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthForm, AuthFormValue} from '../../components/auth-form/auth-form';
+import { AuthService } from '../../services/auth';
 
 @Component({
-    imports: [],
-    selector: 'app-sign-up',
-    styleUrl: './sign-up.scss',
-    templateUrl: './sign-up.html',
+selector: 'app-sign-up',
+standalone: true,
+imports: [AuthForm],
+templateUrl: './sign-up.html',
+styleUrl: './sign-up.scss',
 })
-export class SignUp {}
+export class SignUp {
+private authService = inject(AuthService);
+private router = inject(Router);
+
+async signUp(value: AuthFormValue): Promise<void> {
+    if (!value.name) return;
+
+    const { data, error } = await this.authService.signUp(
+    value.name,
+    value.email,
+    value.password
+    );
+
+    if (error) {
+    console.error('Registrierung fehlgeschlagen:', error);
+    return;
+    }
+
+    if (data.session) {
+        await this.router.navigate(['/summary']);
+    } else {
+        await this.router.navigate(['/log-in']);
+    }
+}
+}
