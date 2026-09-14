970859159843fb5a06f17eb24edbf6c99d5cba4e
Salih Derin <derin.salih@gmx.de>
2026-09-15 00:09:32 +0200

    fix: delete  import

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,11 @@ import { Router ,RouterOutlet } from '@angular/router';
 import { Aside } from './layout/aside/aside';
 import { Header } from './layout/header/header';
 import { ContactService } from './services/contact';
-import { Summary } from './pages/summary/summary';
-import { LogIn } from './pages/log-in/log-in';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Aside, Summary],
+  imports: [RouterOutlet, Header, Aside],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
