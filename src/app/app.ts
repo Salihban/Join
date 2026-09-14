30886fb250dd7465daa6e85530f728b8ed965128
Axel-ne <axel-neugebauer@gmx.net>
2026-09-14 15:26:12 +0200

    summary mobile structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Aside } from './layout/aside/aside';
 import { Header } from './layout/header/header';
 import { ContactService } from './services/contact';
+import { Summary } from './pages/summary/summary';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Aside],
+  imports: [RouterOutlet, Header, Aside, Summary],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
