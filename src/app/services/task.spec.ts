c4e46a6c54cd1efa3d73ac6f160cd7afa3be8f32
frontendfuchs <adriana.letitia@yahoo.com>
2026-09-21 14:43:46 +0200

    fix: task spec.ts

--- src/app/services/task.spec.ts
+++ src/app/services/task.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Task } from './task';
+import { Task, TaskService } from './task';
 
 describe('Task', () => {
-    let service: Task;
+    let service: TaskService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
-        service = TestBed.inject(Task);
+        service = TestBed.inject(TaskService);
     });
 
     it('should be created', () => {
